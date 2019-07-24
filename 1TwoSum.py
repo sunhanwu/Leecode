4b3edb3be51600a86c4c3d0d4bedbedfedda3dbe
Shw <1965190613@qq.com>
2019-07-24 22:07:14 +0800

    完成twocum

--- 1TwoSum.py
+++ 1TwoSum.py
@@ -1,3 +1,11 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]):
-        return
+    # 思路就是暴露寻找
+    def twoSum(self, nums,target):
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums:
+                idx = nums.index(target - nums[i])
+                if idx == i:
+                    pass # 注意样例[3,2,4],6中，不能是[0,0],只能是[1,2]，所以需要判断下表是否一致
+                else:
+                    return [i,nums.index(target - nums[i])]
+
